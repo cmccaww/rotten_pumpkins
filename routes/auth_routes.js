5bd25e298d78df0000bcc4ed335d40c22719797f
Jese310S <jess.sole710@gmail.com>
2020-10-19 18:09:28 +1100

    finished day 1

--- routes/auth_routes.js
+++ routes/auth_routes.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const {registerCreate, registerNew , logOut, loginNew, loginCreate} = require('../controllers/auth_controller')
-// const {authRedirect} = require("../middleware/auth_middleware")
+const {authRedirect} = require("../middleware/auth_middleware")
 
 
-// router.get('/register', authRedirect, registerNew)
-router.get('/register', registerNew)
+router.get('/register', authRedirect, registerNew)
+// router.get('/register', registerNew)
 
 router.post('/register', registerCreate)
 
 router.get('/logout', logOut)
 
-// router.get("/login", authRedirect, loginNew)
-router.get("/login", loginNew)
+router.get("/login", authRedirect, loginNew)
+// router.get("/login", loginNew)
 router.post("/login", loginCreate)
 // changes
 
