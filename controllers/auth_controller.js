38bef937b119fd2800ba1986b1a0b89a88631977
Jese310S <jess.sole710@gmail.com>
2020-10-19 13:58:41 +1100

    add login and register routes and handlers

---
+++ controllers/auth_controller.js
@@ -0,0 +1,50 @@
+const UserModel = require("../models/user");
+
+const registerNew = (req,res) => {
+    res.render("auth/register")
+}
+
+const registerCreate = async (req, res) =>{
+    //    res.json(req.body)
+    // save the req.body to database user collection
+    const {email, password} = req.body
+    const user = await UserModel.create({email, password})
+    // attach the registered user to the session
+    req.session.user = user
+    console.log(req.session.user)
+    res.redirect("/dashboard")
+}
+
+const logOut =(req,res) =>{
+    req.session.destroy(() => {
+        res.redirect("/")
+    })
+}
+
+const loginNew = (req,res) => {
+        res.render("auth/login")
+}
+const loginCreate = async (req,res) => {
+    // res.json(req.body)
+    // fetch the user
+    // verify password and redirect to dashboard
+    const {email, password} = req.body
+    const user = await UserModel.findOne({email})
+    if(!user){
+        return res.render("auth/login", {error: "invalid email and password" })
+    }
+    const valid = await user.verifyPassword(password)
+    if(!valid){
+        return res.render("auth/login", {error: "invalid password" })
+    }
+    req.session.user = user
+    res.redirect("/dashboard", {email: user.email})
+}
+
+module.exports = {
+    registerNew,
+    registerCreate,
+    logOut,
+    loginNew,
+    loginCreate
+}
