7a98e198f388268cfb4e5e9a71a8815154408223
Sevda Amini <sevda.amini@hotmail.com>
2020-10-19 15:17:51 +1000

    some changes!

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const exphbs  = require('express-handlebars');
 const path = require('path');
-
+const pageRouter = require('./routes/page_routes');
+const authRouter = require('./routes/auth_routes');
  
 const app = new express();
 
@@ -15,9 +16,21 @@ app.get('/', function (req, res) {
     res.render('home');
 });
 
+app.get('/register', function (req, res) {
+    res.render('auth/register');
+});
+
+app.get('/login', function (req, res) {
+    res.render('auth/login');
+});
+
 app.get('/search', function (req, res) {
     res.render('search');
 });
+
+app.use("/", pageRouter)
+// app.use('/posts', postRouter);
+app.use("/user", authRouter)
  
 app.listen(3000, () => {
     console.log('App listening on port 3000')
