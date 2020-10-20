fe47204d3e05b2b25ce885bf085fd98e7c578623
Sevda Amini <sevda.amini@hotmail.com>
2020-10-21 08:48:13 +1000

    still debugging

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,9 +1,11 @@
+// const express = require('express');
+// const app = new express();
 
 const Review = require('../models/review');
 
-module.exports = function(app) {
+module.exports = function(app, Review) {
 
-app.get('/', (req, res) => {
+app.get('/reviews', (req, res) => {
     Review.find()
       .then(reviews => {
         res.render('reviews-index', {reviews: reviews});
@@ -15,73 +17,4 @@ app.get('/', (req, res) => {
 
 }
 
-// Index. 
-//Find method returns a Promise.A Promise is an object that represents represents a value that will be provided in the future. 
-app.get('/reviews', (req, res) => {
-  Review.find()
-    .then(reviews => {
-      res.render('reviews-index', { reviews: reviews });
-    })
-    .catch(err => {
-      console.log(err);
-    })
-})
-
-//creating NEW reviews
-app.get('/reviews/new', (req, res) => {
-  res.render('reviews-new', {title: "New Review"});
-})
-
-// CREATE
-app.post('/reviews', (req, res) => {
-  Review.create(req.body).then((review) => {
-    console.log(review)
-    res.redirect(`/reviews/${review._id}`) // Redirect to reviews/:id
-  }).catch((err) => {
-    console.log(err.message)
-  })
-})
-
-
-// SHOW
-// TODO RENDERING IS NOT WORKING, NO FIELDS OF SAVED OBJECTS ARE VISIBLE.
-app.get('/reviews/:id', (req, res) => {
-  Review.findById(req.params.id).then((review) => {
-    res.render('reviews-show', { review: review })
-  }).catch((err) => {
-    console.log(err.message);
-  })
-})
-
-
-// EDIT
-app.get('/reviews/:id/edit', (req, res) => {
-  Review.findById(req.params.id, function(err, review) {
-    res.render('reviews-edit', {review: review, title: "Edit Review"});
-  })
-})
-
-// UPDATE
-app.put('/reviews/:id', (req, res) => {
-  Review.findByIdAndUpdate(req.params.id, req.body)
-    .then(review => {
-      res.redirect(`/reviews/${review._id}`)
-    })
-    .catch(err => {
-      console.log(err.message)
-    })
-})
-
-
-//DELETE
-
-
-app.delete('/reviews/:id', function (req, res) {
-  console.log("DELETE review")
-  ReviewModel.findByIdAndRemove(req.params.id).then((review) => {
-    res.redirect('/');
-  }).catch((err) => {
-    console.log(err.message);
-  })
-})
 
--- models/review.js
+++ models/review.js
@@ -1,3 +1,5 @@
+
+
 const mongoose = require('mongoose');
 
 const Review = mongoose.model('Review', {
