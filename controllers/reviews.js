decbb19045129f2d18fc8ed3d48fa012fe2d7055
Sevda Amini <sevda.amini@hotmail.com>
2020-10-20 21:16:53 +1000

    debugging

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,5 +1,6 @@
 
 const Review = require('../models/review');
+
 module.exports = function(app) {
 
 app.get('/', (req, res) => {
@@ -14,18 +15,73 @@ app.get('/', (req, res) => {
 
 }
 
+// Index. 
+//Find method returns a Promise.A Promise is an object that represents represents a value that will be provided in the future. 
+app.get('/reviews', (req, res) => {
+  Review.find()
+    .then(reviews => {
+      res.render('reviews-index', { reviews: reviews });
+    })
+    .catch(err => {
+      console.log(err);
+    })
+})
+
+//creating NEW reviews
+app.get('/reviews/new', (req, res) => {
+  res.render('reviews-new', {title: "New Review"});
+})
+
+// CREATE
+app.post('/reviews', (req, res) => {
+  Review.create(req.body).then((review) => {
+    console.log(review)
+    res.redirect(`/reviews/${review._id}`) // Redirect to reviews/:id
+  }).catch((err) => {
+    console.log(err.message)
+  })
+})
 
 
-// module.exports = function (app, Review) {
+// SHOW
+// TODO RENDERING IS NOT WORKING, NO FIELDS OF SAVED OBJECTS ARE VISIBLE.
+app.get('/reviews/:id', (req, res) => {
+  Review.findById(req.params.id).then((review) => {
+    res.render('reviews-show', { review: review })
+  }).catch((err) => {
+    console.log(err.message);
+  })
+})
 
-//   app.get('/', (req, res) => {
-//     Review.find()
-//       .then(reviews => {
-//         res.render('reviews-index', {reviews: reviews});
-//       })
-//       .catch(err => {
-//         console.log(err);
-//       });
-//   });
 
-// }
+// EDIT
+app.get('/reviews/:id/edit', (req, res) => {
+  Review.findById(req.params.id, function(err, review) {
+    res.render('reviews-edit', {review: review, title: "Edit Review"});
+  })
+})
+
+// UPDATE
+app.put('/reviews/:id', (req, res) => {
+  Review.findByIdAndUpdate(req.params.id, req.body)
+    .then(review => {
+      res.redirect(`/reviews/${review._id}`)
+    })
+    .catch(err => {
+      console.log(err.message)
+    })
+})
+
+
+//DELETE
+
+
+app.delete('/reviews/:id', function (req, res) {
+  console.log("DELETE review")
+  ReviewModel.findByIdAndRemove(req.params.id).then((review) => {
+    res.redirect('/');
+  }).catch((err) => {
+    console.log(err.message);
+  })
+})
+
--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const express = require('express');
 const exphbs  = require('express-handlebars');
 const path = require('path');
 const routes = require('./routes/app_routes.js');
+const reviews = require('./controllers/reviews')(app);
 const mongoose = require('mongoose');
 const session = require('express-session')
 mongoose.connect('mongodb://localhost/rotten-pumpkins', { useNewUrlParser: true });
-const ReviewModel = require('./models/review')
+// const Review = require('./models/review')
+
 
 // INITIALIZE BODY-PARSER AND ADD IT TO APP
 const bodyParser = require('body-parser');
@@ -15,6 +17,8 @@ const bodyParser = require('body-parser');
 
 const methodOverride = require('method-override')
 const app = new express();
+
+app.use(methodOverride('_method'))
 
 
 // The following line must appear AFTER const app = express() and before your routes!
@@ -45,74 +49,3 @@ app.listen(3000, (err) => {
 });
 
 
-// review section
-
-
-// Index. Find method returns a Promise. A Promise is an object that represents represents a value that will be provided in the future. 
-app.get('/reviews', (req, res) => {
-  ReviewModel.find()
-    .then(reviews => {
-      res.render('reviews-index', { reviews: reviews });
-    })
-    .catch(err => {
-      console.log(err);
-    })
-})
-
-//creating NEW reviews
-app.get('/reviews/new', (req, res) => {
-  res.render('reviews-new', {title: "New Review"});
-})
-
-// CREATE
-app.post('/reviews', (req, res) => {
-  ReviewModel.create(req.body).then((review) => {
-    console.log(review)
-    res.redirect(`/reviews/${review._id}`) // Redirect to reviews/:id
-  }).catch((err) => {
-    console.log(err.message)
-  })
-})
-
-
-// SHOW
-// TODO RENDERING IS NOT WORKING, NO FIELDS OF SAVED OBJECTS ARE VISIBLE.
-app.get('/reviews/:id', (req, res) => {
-  ReviewModel.findById(req.params.id).then((review) => {
-    res.render('reviews-show', { review: review })
-  }).catch((err) => {
-    console.log(err.message);
-  })
-})
-
-
-// EDIT
-app.get('/reviews/:id/edit', (req, res) => {
-  ReviewModel.findById(req.params.id, function(err, review) {
-    res.render('reviews-edit', {review: review, title: "Edit Review"});
-  })
-})
-
-// UPDATE
-app.put('/reviews/:id', (req, res) => {
-  ReviewModel.findByIdAndUpdate(req.params.id, req.body)
-    .then(review => {
-      res.redirect(`/reviews/${review._id}`)
-    })
-    .catch(err => {
-      console.log(err.message)
-    })
-})
-
-
-//DELETE
-
-
-app.delete('/reviews/:id', function (req, res) {
-  console.log("DELETE review")
-  ReviewModel.findByIdAndRemove(req.params.id).then((review) => {
-    res.redirect('/');
-  }).catch((err) => {
-    console.log(err.message);
-  })
-})
