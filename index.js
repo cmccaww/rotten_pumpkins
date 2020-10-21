7454e6137469d456d1edb6b848d56e25aa45fb97
Jese310S <jess.sole710@gmail.com>
2020-10-21 13:08:37 +1100

    fix user flow

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ app.get('/reviews/new', (req, res) => {
 app.post('/reviews', (req, res) => {
   ReviewModel.create(req.body).then((review) => {
     console.log(review)
-    res.redirect(`/reviews/${review._id}`) // Redirect to reviews/:id
+    res.redirect('/reviews') // Redirect to reviews/:id
   }).catch((err) => {
     console.log(err.message)
   })
