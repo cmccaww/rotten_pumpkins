551449e899cf1b7984e1b47faa1425fa596da1db
Caitlin <caitlin.mccawley@gmail.com>
2020-10-21 10:13:57 +1000

    cleaned up index

--- index.js
+++ index.js
@@ -7,15 +7,14 @@ const mongoose = require('mongoose');
 const session = require('express-session')
 mongoose.connect('mongodb://localhost/rotten-pumpkins', { useNewUrlParser: true });
 const ReviewModel = require('./models/review')
-
 // INITIALIZE BODY-PARSER AND ADD IT TO APP
 const bodyParser = require('body-parser');
-
-
 
 const methodOverride = require('method-override')
 const app = new express();
 
+app.use(express.static('public'));
+app.use(express.urlencoded())
 
 // The following line must appear AFTER const app = express() and before your routes!
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -44,9 +43,7 @@ app.listen(3000, (err) => {
     console.log('App listening on port 3000')
 });
 
-
 // review section
-
 
 // Index. Find method returns a Promise. A Promise is an object that represents represents a value that will be provided in the future. 
 app.get('/reviews', (req, res) => {
@@ -106,8 +103,6 @@ app.put('/reviews/:id', (req, res) => {
 
 
 //DELETE
-
-
 app.delete('/reviews/:id', function (req, res) {
   console.log("DELETE review")
   ReviewModel.findByIdAndRemove(req.params.id).then((review) => {
@@ -116,18 +111,6 @@ app.delete('/reviews/:id', function (req, res) {
     console.log(err.message);
   })
 })
-const express = require('express');
-const exphbs  = require('express-handlebars');
-const path = require('path');
-const routes = require('./routes/app_routes.js');
-const mongoose = require('mongoose')
-const session = require('express-session')
-
- 
-const app = new express();
-
-app.use(express.static('public'));
-app.use(express.urlencoded())
 
 // use sessions for tracking logins
 app.use(session({
@@ -142,7 +125,7 @@ app.use( function(req,res, next){
     next();
 })
 
- // include routes
+// include routes
 app.use('/', routes);
 
 //mongoose connect
@@ -150,28 +133,3 @@ mongoose.connect("mongodb://localhost:27017/rotten_pumpkins");
 const db = mongoose.connection;
 //mongo error
 db.on('error', console.error.bind(console, 'connection error:'));
-
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
-
-app.get('/', function (req, res) {
-    res.render('home');
-});
-
-app.get('/register', function (req, res) {
-    res.render('auth/register');
-});
-
-app.get('/login', function (req, res) {
-    res.render('auth/login');
-});
-
-app.get('/search', function (req, res) {
-    res.render('search');
-});
-
-
- 
-app.listen(3000, () => {
-    console.log('App listening on port 3000')
-});
